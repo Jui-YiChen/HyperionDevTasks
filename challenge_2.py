9539bc0c9ef8b63b6d95ecaf75067f85783da719
Jui-Yi Chen <zcbtjc9@ucl.ac.uk>
2024-01-09 13:29:12 +0000

    delete

--- challenge_2.py
+++
@@ -1,8 +0,0 @@
-string_fav = input("Please enter your favourate reseraunt name:")
-int_fav = int(input("Please enter your favourate number"))
-print(int(string_fav))
-print(int(int_fav))
-
-''' When use int for a string, it shows a value error: invalid literal for int()
-with base 10. I think because the system canot recognize the letters in an int()
-casting.'''
