41e1ff519dcd9d8aedc6b4e183dfaec8eb7f2aa6
Jui-Yi Chen <154987642+Jui-YiChen@users.noreply.github.com>
2024-01-09 13:04:57 +0000

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,15 @@
+integer1 = input("Please enter the first integer:") #input ints
+integer2 = input("Please enter the second integer:")
+integer3 = input("Please enter the third integer:")
+
+sum = int(integer1) + int(integer2) + int(integer3) #change str to int
+print(sum)
+
+first_minus_second = int(integer1) - int(integer2) # first - second
+print(first_minus_second)
+
+third_multiply_first = int(integer3)*int(integer1) #first multipy third
+print(third_multiply_first)
+
+sum_divided_by_third = sum/ int(integer3) #sum divided by third
+print(sum_divided_by_third)
---
+++ replace.py
@@ -0,0 +1,10 @@
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog."
+
+replacesentense = sentence.replace("!"," ") #replace ! with " "
+print(replacesentense)
+
+cap_replacesentense = replacesentense.upper() #change to upper case
+print(cap_replacesentense)
+
+reverse_cap_replacesentense = cap_replacesentense[::-1] #reverse string
+print(reverse_cap_replacesentense)
